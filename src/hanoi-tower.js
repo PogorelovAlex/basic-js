d93384500198f33554b826699edec7c13c922c99
Alexei Pogorelov <alexeipogorelovbn@yandex.ru>
2020-10-30 14:03:50 +0300

    feat:add function transform-array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -4,7 +4,7 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   let result = {
     turns:0,
     seconds:0,
-  }
+  };
 
   result.turns = 2**disksNumber-1;
   result.seconds = Math.floor(3600 / turnsSpeed * result.turns);
