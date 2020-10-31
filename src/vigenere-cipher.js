bef92a122e0b5ddf4ff7d5d23cabce68c1db89f9
Alexei Pogorelov <alexeipogorelovbn@yandex.ru>
2020-10-31 15:47:44 +0300

    feat:add recursive and vigenere function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,109 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+ constructor(mode = true) {
+    this.mode = mode;
+    
   }
+ 
+  encrypt(message, key) {
+    const alphabet = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    message = message.toUpperCase();
+    let resCode = [];
+    let keys = key.toUpperCase();
+    let j = 0;
+    if (!this.mode) {message = message.split("").reverse()};
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let index =
+          (alphabet.indexOf(message[i]) + alphabet.indexOf(keys[j])) % 26;
+        let tmp = alphabet[index];
+        resCode.push(tmp);
+        j++;
+        if (j == keys.length) j = 0;
+      } else resCode.push(message[i]);
+    }
+    return resCode.join("");
+  }
+
+  decrypt(message, key) {
+    const alphabet = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    message = message.toUpperCase();
+    let resCode = [];
+    let keys = key.toUpperCase();
+    let j = 0;
+    if (!this.mode) message = message.split("").reverse();
+
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let index =
+          (alphabet.indexOf(message[i]) - alphabet.indexOf(keys[j]) + 26) % 26;
+        let tmp = alphabet[index];
+        resCode.push(tmp);
+        j++;
+        if (j == keys.length) j = 0;
+      } else resCode.push(message[i]);
+    }
+    return resCode.join("");
+  }
+
+  
 }
 
 module.exports = VigenereCipheringMachine;
+
